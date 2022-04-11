2b174ee4385bb31e1532f53af3bf60ac9356ba5a
Ritesh Patel <riteshzeronity@gmail.com>
2022-04-11 21:23:35 +0530

    initital commit

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<?php include_once('header.php'); ?>
+
+<section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>Articles</h3></header>
+            <?php
+            // retrive posts
+            ?>
+        </div>
+    </section>
+    
+</body>
+</html>
