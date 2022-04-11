6674ede608a18279c2eaf97e2dca795454685069
Ritesh Patel <riteshzeronity@gmail.com>
2022-04-11 21:47:53 +0530

    added add_article and signin page

---
+++ add_article.php
@@ -0,0 +1,17 @@
+<?php include_once('header.php') ?>
+<section class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>Add a new article</h3></header>
+            <form action="#" method="post">
+            <div class="form-group">
+                    <input class="text" name="heading" id="hdg" placeholder="Your Article Heading">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" name="body" id="atl" rows="5" placeholder="Your Article"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Add Article</button>
+            </form>
+        </div>
+</section>
+</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,20 @@
+<?php include_once('header.php') ?>
+
+<div class="row">
+<div class="col-md-6 col-md-offset-3">
+        <h3>Admin Panel Sign In</h3>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="email">Your E-mail</label>
+                <input class="form-control" type="text" name="email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="text" name="password" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Sign In</button>
+        </form>
+</div>
+</div>
+</body>
+</html>
