48ff10719c437933b44277ed1101e34be6c7f871
Ritesh Patel <riteshzeronity@gmail.com>
2022-04-18 13:36:44 +0530

    Setup the view_post pages

--- home.php
+++ home.php
@@ -4,29 +4,30 @@ include_once('header.php');
 ?>
 
 <section class="row posts">
-        <div class="col-md-6 col-md-offset-3">
-            <header><h3>Articles</h3></header>
-            
-            <div class="col-md-8 post" style="margin-bottom:24px">
-            <?php
-            $sql = "SELECT * FROM articles";
-            $result = mysqli_query($con, $sql);
+    <div class="col-md-6 col-md-offset-3">
+        <header><h3>Articles</h3></header>
+        
+        <div class="col-md-8 post" style="margin-bottom:24px">
+        <?php
+        $sql = "SELECT * FROM articles";
+        $result = mysqli_query($con, $sql);
 
-            if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-                while($row = mysqli_fetch_assoc($result)) {
-                    $heading = $row['heading'];
-                    $date = $row['creationdate'];
-                    echo "<h4><a href=''>$heading</a></h4><div class='info'>
-                    <i>Posted by Ritesh on $date</i></div>";
-                }
-            } else {
-                echo "0 results";
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                $postid = $row['id'];
+                $heading = $row['heading'];
+                $date = $row['creationdate'];
+                echo "<h4><a href='view_post.php?id=$postid'>$heading</a></h4><div class='info'>
+                <i>Posted by Ritesh on $date</i></div>";
             }
-            ?>
-            
-        </div>
-    </section>
+        } else {
+            echo "0 results";
+        }
+        ?>
+        
+    </div>
+</section>
     
 </body>
 </html>
--- view_post.php
+++ view_post.php
@@ -1,32 +1,29 @@
-<?php include_once('header.php') ?>
+<?php
+include_once('database_connection.php');
+include_once('header.php') 
+?>
 
 <div class="col-md-6 col-md-offset-3">
-            <header><h3>How to Think More Effectively</h3></header>
+    <?php
+    $postid = $_GET['id'];
+
+    $sql = "SELECT * FROM articles where id = $postid";
+    $result = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            $heading = $row['heading'];
+            $date = $row['creationdate'];
+            $content = $row['content'];
+            echo "<h3>$heading</h3>";
                     
-                    <div class="info">
-                        <i>Posted by Ritesh on 01-04-22</i>
-                    </div>
+            echo "<div class='info'><i>Posted by Ritesh on $date</i></div>";
 
-                    <div class="body" style="margin-top:24px">
-                        No skill is more valuable and harder to come by than the ability to critically think through problems. Schools don’t teach you a method of thinking. Thinking is one of those things that can be learned but can’t be taught.
-
-When it comes to thinking the mind has an optimal way to be operated. When operated correctly you’ll find yourself with plenty of free time. When operated incorrectly, most of your time will be consumed correcting mistakes.
-
-Good decisions create time, bad ones consume it. Good initial decisions pay dividends for years, allowing abundant free time and low stress. Poor decisions, on the other hand, consume time, increase anxiety, and drain us of energy.
-
-But how can we learn how to think?
-
-For the answer we turn to Solitude and Leadership, a lecture given by William Deresiewicz. The entire essay is worth reading (and re-reading).
-
-Learning How To Think
-Let’s start with how you don’t learn to think. A study by a team of researchers at Stanford came out a couple of months ago. The investigators wanted to figure out how today’s college students were able to multitask so much more effectively than adults. How do they manage to do it, the researchers asked? The answer, they discovered—and this is by no means what they expected—is that they don’t. The enhanced cognitive abilities the investigators expected to find, the mental faculties that enable people to multitask effectively, were simply not there. In other words, people do not multitask effectively. And here’s the really surprising finding: the more people multitask, the worse they are, not just at other mental abilities, but at multitasking itself.
-
-One thing that made the study different from others is that the researchers didn’t test people’s cognitive functions while they were multitasking. They separated the subject group into high multitaskers and low multitaskers and used a different set of tests to measure the kinds of cognitive abilities involved in multitasking. They found that in every case the high multitaskers scored worse. They were worse at distinguishing between relevant and irrelevant information and ignoring the latter. In other words, they were more distractible. They were worse at what you might call “mental filing”: keeping information in the right conceptual boxes and being able to retrieve it quickly. In other words, their minds were more disorganized. And they were even worse at the very thing that defines multitasking itself: switching between tasks.
-
-Multitasking, in short, is not only not thinking, it impairs your ability to think. Thinking means concentrating on one thing long enough to develop an idea about it. Not learning other people’s ideas, or memorizing a body of information, however much those may sometimes be useful. Developing your own ideas. In short, thinking for yourself. You simply cannot do that in bursts of 20 seconds at a time, constantly interrupted by Facebook messages or Twitter tweets, or fiddling with your iPod, or watching something on YouTube.
-
-I find for myself that my first thought is never my best thought. My first thought is always someone else’s; it’s always what I’ve already heard about the subject, always the conventional wisdom. It’s only by concentrating, sticking to the question, being patient, letting all the parts of my mind come into play, that I arrive at an original idea. By giving my brain a chance to make associations, draw connections, take me by surprise. And often even that idea doesn’t turn out to be very good. I need time to think about it, too, to make mistakes and recognize them, to make false starts and correct them, to outlast my impulses, to defeat my desire to declare the job done and move on to the next thing.
-
-I used to have students who bragged to me about how fast they wrote their papers. I would tell them that the great German novelist Thomas Mann said that a writer is someone for whom writing is more difficult than it is for other people. The best writers write much more slowly than everyone else, and the better they are, the slower they write. James Joyce wrote Ulysses, the greatest novel of the 20th century, at the rate of about a hundred words a day—half the length of the selection I read you earlier from Heart of Darkness—for seven years. T. S. Eliot, one of the greatest poets our country has ever produced, wrote about 150 pages of poetry over the course of his entire 25-year career. That’s half a page a month. So it is with any other form of thought. You do your best thinking by slowing down and concentrating.
-                    </div>
+            echo "<div class='body' style='margin-top:24px'>$content</div>";
+        }
+    } else {
+        echo "0 results";
+    }
+    ?>
 </div>
